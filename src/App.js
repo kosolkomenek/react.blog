50bdcc54571e6753940046c64c31dbf2362e45f9
DocRagnarok <106695471+DocRagnarok@users.noreply.github.com>
2022-07-06 01:59:00 +0700

    reactup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Home from './Pages/Home/HomePage';
+import About from './Pages/About/AboutPage';
+import Contact from './Pages/Contact/ContactPage';
+import Nav from './Component/Nav';
+import {Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Nav />
+   <div className="container">
+   <Route exact={true} path="/" component={Home} />
+
+
+   </div>
     </div>
   );
 }
